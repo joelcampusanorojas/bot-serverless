d3dca8f8d58a59dd4f5a5b4886b6692f518a0e12
Joel <joel.campusano.rojas@gmail.com>
2020-09-27 10:03:59 -0300

    feat: Incluye Search Services
    Incluye the integrated with search services
    for message user

--- HttpTrigger/__init__.py
+++ HttpTrigger/__init__.py
@@ -4,13 +4,15 @@ import os
 import requests
 
 from .luis import get_luis
+from .search import get_search
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
 
     req_body = req.get_json()
     text = req_body.get('text').lower()
     luis_response = get_luis(text)
+    search_response = get_search(text)
     #logging.info(luis_response)
     
-    return func.HttpResponse(f"Hello, your said: {text}. The intent in LUIS is: {luis_response}.")
+    return func.HttpResponse(f"Hello, your said: {text}. The intent in LUIS is: {luis_response}. The search result is : {search_response}")
 
---
+++ HttpTrigger/search.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import http.client, urllib.parse, json, time
+import json
+import requests
+import logging
+
+def get_search(question):
+    try:
+
+        host = 'bot-serverless-search.search.windows.net'
+        endpoint_key = '7C9CB5854474711F77CF30D385BEF4BC'
+        api_version = '2020-06-30'
+        path = '/indexes/cosmosdb-index/'
+
+        question = question.lower()
+        
+        answer = ''
+        score = 0
+
+        headers = {
+            'api-key': endpoint_key,
+            'Content-Type': 'application/json',
+        }
+
+        content = {
+        "api-version": api_version,
+        "queryType": "simple",
+        "search": question,
+        "$top": 1,
+        "orderby": "Rating desc"
+        }
+
+        payload = ''
+        params = urllib.parse.urlencode(content)
+        url = path + 'docs?' + params
+
+        conn = http.client.HTTPSConnection(host)
+        conn.request("GET", url, payload, headers)
+        response = conn.getresponse()
+        
+        
+
+        if(response.status == 200):
+            result_json = response.read()
+            result_json = json.loads(result_json)
+            value = result_json["value"]
+            
+            if(len(value) > 0):
+                value = value[0]
+                score = value["@search.score"]
+                question = value["questions"]
+                answer = value["answer"]    
+
+        answer = str(answer)
+        logging.info(f'*** {str(answer)}')
+        return {answer}
+
+    except Exception as e:
+        # Display the error string.
+        print(f'{e}')
